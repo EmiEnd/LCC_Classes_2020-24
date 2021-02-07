62717ea48c4c3d7eed7b02eb3a39ab0527dcecaa
Emiliano Ibarra <emiend@users.noreply.github.com>
2021-02-06 17:59:27 -0700

    Revert "New codes and cycles added to Recursos.cpp"
    This reverts commit ce82609f82f90c1c5ea8d5d7a63ff4901a0332fd.

--- Assigned Projects/Los_ceros_no_valen.cpp
+++ Assigned Projects/Los_ceros_no_valen.cpp
@@ -2,7 +2,7 @@
 Nombre: Los_ceros_no_valen.cpp
 Autor: Emiliano Tadeo Carlos Ibarra
 Fecha: 29-01-2021
-Descripcion: Dados tres numeros reales, correspondientes a los tres coeficientes de una funcion cuadratica de la forma f(x) = ax^2 + bx + c se encuentran sus raices. 
+Descripcion: Dados tres números reales, correspondientes a los tres coeficientes de una función cuadrática de la forma f(x) = ax^2 + bx + c se encuentran sus raices. 
 */
 
 #include <iostream>
@@ -13,21 +13,17 @@ using namespace std;
 
 int main()
 {
-    float a,b,c,x1,x2,discriminante,parte_imaginaria;
+    float a,b,c,x1,x2,discriminante,parte_real,parte_imaginaria;
     unsigned int cero;
     cout << "Este programa encuentra las ra" << (char)161 <<"ces de una operaci" << (char)162 << "n con forma: " << endl;
     cout << "       f(x)=ax^2+bx+c" << endl;
-    cout << "Ingrese los tres coeficientes en orden a,b,c." << endl;
-    cout << "   a: ";
-    cin >> a;
+    cout << "Ingrese los tres coeficientes en orden a,b,c: " << endl;
+    cin >> a >> b >> c;
+    
     while (a==0){
-		cout << "El valor de a no puede ser 0, reingreselo: ";
+		cout << "El valor de a no puede ser 0, reingreselo: " << endl;
 		cin >> a;
-	}    
-    cout << "   b: ";
-    cin >> b;
-    cout << "   c: ";
-    cin >> c;
+	}
 
     discriminante = (b*b)-4*a*c;
 
@@ -40,15 +36,14 @@ int main()
         cout << "La ra" << (char)161 << "z es x= " << x1 << endl;
     }else{
         x1= -b/(2*a);
-        parte_imaginaria= sqrt(-discriminante)/(2*a);
-        if(x1==-0 || x1==0){
-        	cout << "Las ra" << (char)161 << "ces son x1= " << parte_imaginaria << "i y x2= " << (-1*parte_imaginaria) << "i" << endl;
-		}else{
-			if (parte_imaginaria<0){
-				parte_imaginaria=(-1*parte_imaginaria);
-			}
-			cout << "Las ra" << (char)161 << "ces son x1= " << x1 << (char)241 << parte_imaginaria << endl;
-		}
+        x2= sqrt(-discriminante)/(2*a);
+        if (x1==-0)
+        {
+            cero=x1;
+            cout << "Las ra" << (char)161 << "ces son x1= " << cero << " y x2= " << x2 << "i" << endl;
+        }else{
+            cout << "Las ra" << (char)161 << "ces son x1= " << x1 << " y x2= " << x2 << "i" << endl;
+        }
     }   
 
     system("pause");
