ce82609f82f90c1c5ea8d5d7a63ff4901a0332fd
Emiliano Ibarra <emiend@users.noreply.github.com>
2021-02-06 17:58:32 -0700

    New codes and cycles added to Recursos.cpp
    Added cycles to Recursos.cpp and added a new homework Que_dias_aquellos.cpp which uses enumerations.

---
+++ Assigned Projects/template.cpp
@@ -0,0 +1,18 @@
+/*
+Nombre: .cpp
+Autor: Emiliano Tadeo Carlos Ibarra
+Fecha: 
+Descripcion: 
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+
+    system("pause");
+    return 0;
+}
